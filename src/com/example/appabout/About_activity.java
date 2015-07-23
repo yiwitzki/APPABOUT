8dbc629f4868871b4e44bb7e2a5874ac1a4af636
yiwitzki <byebyehighbury@163.com>
2015-07-23 21:54:20 +0800

    mainlayout done

---
+++ src/com/example/appabout/About_activity.java
@@ -0,0 +1,44 @@
+package com.example.appabout;
+
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class About_activity extends Activity 
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupDialogActionBar();
+    }
+    
+    private void setupDialogActionBar()
+	{
+		getActionBar().setBackgroundDrawable(new ColorDrawable(Color.rgb(0x1E, 0x90, 0xFF)));
+		getActionBar().setDisplayShowHomeEnabled(false);
+		getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); 
+		getActionBar().setCustomView(R.layout.tp_app_about_actionbar);
+		
+		TextView title = (TextView)findViewById(R.id.tp_sendpostactivity_actionbar_title);
+		LinearLayout back = (LinearLayout)findViewById(R.id.tp_app_about_actionbar_back);
+		back.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				finish();
+			}
+		});
+	}
+}
