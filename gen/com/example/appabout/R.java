8b53cd09af08713183e927e5d6232bdc2ecb395b
yiwitzki <byebyehighbury@163.com>
2015-07-24 12:20:00 +0800

    add button in mainactivity

--- gen/com/example/appabout/R.java
+++ gen/com/example/appabout/R.java
@@ -101,7 +101,7 @@ public final class R {
         public static final int tp_weibo_btn=0x7f090000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int tp_about_activity=0x7f030000;
         public static final int tp_app_about_actionbar=0x7f030001;
     }
     public static final class menu {
--- src/com/example/appabout/About_activity.java
+++ src/com/example/appabout/About_activity.java
@@ -8,18 +8,19 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 public class About_activity extends Activity 
 {
-
+	private Button weiBoBtn, weChatBtn, qqBtn, updateBtn, recommandBtn, policyBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tp_about_activity);
         setupDialogActionBar();
     }
     
@@ -41,4 +42,8 @@ public class About_activity extends Activity
 			}
 		});
 	}
+    private void initview()
+    {
+    	
+    }
 }
